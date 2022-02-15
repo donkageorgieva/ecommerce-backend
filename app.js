1b9e1a803c2d76768d5687577f037d7fa1c530f7
donkageorgieva <DoniGeorgieva16@gmail.com>
2022-02-15 12:23:05 +0200

    fix middleware

--- app.js
+++ app.js
@@ -26,10 +26,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  "*", cors();
-  next();
-});
+app.use("*", cors());
 
 app.use("/items", sneakerRoutes);
 app.use("/cart", cartRoutes);
