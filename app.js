1ea44662fe7717074c8149433e381fdb06c1a8a6
donkageorgieva <DoniGeorgieva16@gmail.com>
2022-02-15 12:11:15 +0200

    fix: headers

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, DELETE, PATCH, PUT"
+    "GET, POST, DELETE, PATCH, PUT, OPTIONS"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
